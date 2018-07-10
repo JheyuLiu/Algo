ecf7e420e8efb322c733e04f69b742c5469b1e8c
jheyu_liu <jheyu@jheyu-liude-MacBook-Pro.local>
2018-07-10 21:57:26 +0800

    using map

--- Tree/two-sum-input-is-a-bst.go
+++ Tree/two-sum-input-is-a-bst.go
@@ -1,6 +1,3 @@
-package main
-
-import "fmt"
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -9,53 +6,26 @@ import "fmt"
  *     Right *TreeNode
  * }
  */
-type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
-}
-
-func findTarget(root *TreeNode, k int) bool {
-    var list []int
-    inOrder(root, &list)
-
-    if len(list) < 2 {
+func find(node *TreeNode, k int, tmap *map[int]int) bool {
+    if node == nil {
         return false
     }
 
-    left := 0
-    right := len(list)-1
-    for ; left<right; {
-        sum := list[left]+list[right]
-        if sum > k {
-            right = right-1
-        }else if sum < k {
-            left = left+1
-        }else {
-            return true
-        }
+    num := k-node.Val
+    _, ok := (*tmap)[num]
+    if ok {
+        return true
+    } else {
+        (*tmap)[node.Val] = 0
     }
 
-    return false
+    res_left := find(node.Left, k, tmap)
+    res_right := find(node.Right,k, tmap)
+
+    return res_left || res_right
 }
 
-func inOrder(root *TreeNode, list *[]int) {
-    if root == nil {
-    	return
-    }
-    
-    inOrder(root.Left, list)
-    *list = append(*list, root.Val)
-    inOrder(root.Right, list)
-}
-
-func main() {
-	root := &TreeNode{Val: 2}
-	root.Left = &TreeNode{Val: 3}
-	root.Right = &TreeNode{Val: 3}
-	root.Left.Left = &TreeNode{Val: 2}
-	root.Left.Right = &TreeNode{Val: 4}
-	root.Right.Right = &TreeNode{Val: 7}
-
-	fmt.Println(findTarget(root, 6))
+func findTarget(root *TreeNode, k int) bool {
+    tmap := make(map[int]int)
+    return find(root, k, &tmap)
 }
