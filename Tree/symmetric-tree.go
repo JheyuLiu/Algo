a113fff86e04a20f69e87ea7a8f107982432aced
jheyu_liu <jheyu@jheyu-liude-MacBook-Pro.local>
2018-08-04 17:26:15 +0800

    Symmetric Tree

---
+++ Tree/symmetric-tree.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func help(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil && right != nil ||
+	    left != nil && right == nil {
+	    	return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+
+    return help(left.Left, right.Right) && help(left.Right, right.Left)
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return help(root.Left, root.Right)
+}
