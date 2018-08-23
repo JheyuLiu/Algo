6d55ee6479b9863d4a0f31807fb376d49cefbfd2
jheyu_liu <jheyu@jheyu-liude-MacBook-Pro.local>
2018-08-23 22:03:56 +0800

    Minimum Depth of Binary Tree

---
+++ Tree/minimum-depth-of-binary-tree.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    if root.Left == nil && root.Right == nil {
+        return 1
+    }else if root.Left == nil && root.Right != nil {
+        return minDepth(root.Right) + 1
+    }else if root.Left != nil && root.Right == nil {
+        return minDepth(root.Left) + 1
+    }
+
+    left := minDepth(root.Left) + 1
+    right := minDepth(root.Right) + 1
+    if left < right {
+        return left
+    }
+    return right
+}
