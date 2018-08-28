0a7b9e4ff022ff8edba61767dc2af1be312d7ab5
jheyu_liu <jheyu@jheyu-liude-MacBook-Pro.local>
2018-08-28 23:03:22 +0800

    update

--- LinkedList/middle-of-the-linked-list.go
+++ LinkedList/middle-of-the-linked-list.go
@@ -1,6 +1,3 @@
-package main
-
-import "fmt"
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -8,27 +5,7 @@ import "fmt"
  *     Next *ListNode
  * }
  */
-type ListNode struct {
-	Val int
-	Next *ListNode
-}
-
 func middleNode(head *ListNode) *ListNode {
-    /*var count = 1
-    tmp := head
-    for tmp.Next != nil {
-    	count = count + 1
-    	tmp = tmp.Next
-    }
-
-    var index int
-    for head.Next != nil {
-        if index == count/2 {
-        	break;
-        }
-        index = index + 1
-        head = head.Next
-    }*/
     slow := head
     fast := head
     for fast != nil && fast.Next != nil {
@@ -37,16 +14,4 @@ func middleNode(head *ListNode) *ListNode {
     }
 
     return slow
-}
-
-func main() {
-	head := &ListNode{Val: 1}
-	head.Next = &ListNode{Val: 2}
-	head.Next.Next = &ListNode{Val: 3}
-	head.Next.Next.Next = &ListNode{Val: 4}
-	head.Next.Next.Next.Next = &ListNode{Val: 5}
-    head.Next.Next.Next.Next.Next = &ListNode{Val: 6}
-
-	res_node := middleNode(head)
-	fmt.Println("Node",res_node.Val,"from this list")
 }
